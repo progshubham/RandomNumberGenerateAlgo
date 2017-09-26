4e8d6beb14bad000f782f24984b35a292496dda7
Shubham Kumar Rohit <shubh.java@gmail.com>
2017-09-26 08:08:52 +0530

    new logic

--- app.py
+++ app.py
@@ -19,6 +19,19 @@ app.secret_key = os.urandom(24)
 def index():
 	#define number between 1 to 10
 	number = 6
+	a=1
+	b=2
+	m=5000
+	x=3
+	lst=[]
+	for i in range(10):
+	  x=(a*x+b)%m
+	  lst.append(x)
+	print(lst)
+	if len(lst) != len(set(lst)):
+	    print("it has duplicates but possibly random numbers")
+	else:
+	    print("those were random numbers")
 	random = timeit.Timer('for i in range(1000):i**i').timeit(1)*10032890%100*number
 	print random
 	return str(random)
